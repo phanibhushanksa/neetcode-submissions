45aca97ec09e23f791e402c079c15bd231750bb5
Phani Bhushan Kolla <phanibhushanksa@gmail.com>
2026-06-06 18:24:50 -0400

    Add: merge-intervals - submission-1

---
+++ Data Structures & Algorithms/merge-intervals/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda x: x[0])
+
+
+        result = [intervals[0]]
+
+
+
+        for i in range(1,len(intervals)):
+            current = intervals[i]
+
+            # compare with last element in the result
+            last = result[-1]
+            
+
+            # check for overlap
+
+            if last[1]>=current[0]:
+
+                # if true, extend the existing last element in the result
+                last[1] = max(last[1], current[1])
+
+            else:
+
+                # if no overlap, add the current element
+                result.append(current) 
+
+
+        return result
